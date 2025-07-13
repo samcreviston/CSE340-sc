33221079a032930427d4eb17dbf0163c29eb7f13
Sam Creviston <samuelcreviston@gmail.com>
2025-07-12 23:03:46 -0400

    COMPLETED REGISTER VIEW ROUTE and corrected form contents to be block instead of flex

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -12,4 +12,16 @@ async function buildLogin(req, res, next) {
   })
 }
 
-module.exports = { buildLogin }
+/* ****************************************
+*  Deliver register view
+* *************************************** */
+async function buildRegister(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/register", {
+    title: "Register",
+    nav,
+    errors: null
+    })
+  }
+
+module.exports = { buildLogin, buildRegister }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,10 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 
+//login page route
 router.get("/login", accountController.buildLogin);
+
+//register page route
+router.get("/register", accountController.buildRegister);
 
 module.exports = router;
